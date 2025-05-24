1391e67018891337518a00f3cc89dcd6f5c5cb57
Ibrahim Mohamed <160352487+idevmoo@users.noreply.github.com>
2025-05-24 07:03:13 +0300

    fix height round 2

--- script.js
+++ script.js
@@ -115,3 +115,11 @@ formAction.addEventListener("submit", (event) => {
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+function setVh() {
+      let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+        setVh();
+        window.addEventListener('resize', setVh);
+}
